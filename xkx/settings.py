0fdf58da504312fe8e31c89feb4cca89ca3c716c
Keith-Mpala <keithmpala.km@gmail.com>
2023-02-19 13:25:32 +0100

    railwayapp

--- xkx/settings.py
+++ xkx/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'FinCred',
         'USER':'postgres',
         'PASSWORD':'Drextorluv1*',
-        'HOST':'localhost',
+        'HOST':'127.0.0.1',
         'PORT':'5432'
         
 
